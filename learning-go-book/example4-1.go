c3f18fb0992fe81efa487b4fee16f26b85cb8a67
william <william@freepackets.net>
2022-10-06 20:30:54 +0800

    To page 162 defer

--- learning-go-book/example4-1.go
+++ learning-go-book/example4-1.go
@@ -134,5 +134,93 @@ outer:
 		}
 		fmt.Println()
 	}
-}
 
+	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologist"}
+
+	for _, word := range words {
+		switch size := len(word); size {
+		case 1, 2, 3, 4:
+			fmt.Println(word, "is a short word!")
+		case 5:
+			wordLen := len(word)
+			fmt.Println(word, "is exactly the right length:", wordLen)
+		case 6, 7, 8, 9:
+		default:
+			fmt.Println(word, "is a long word!")
+		}
+	}
+
+loop:
+	for i := 0; i < 10; i++ {
+		switch {
+		case i%2 == 0:
+			fmt.Println(i, "is even")
+		case i%3 == 0:
+			fmt.Println(i, "is divisible by 3 but not 2")
+		case i%7 == 0:
+			fmt.Println("exit the loop!")
+			break loop
+		default:
+			fmt.Println(i, "is boring")
+		}
+	}
+
+	words = []string{"hi", "salutations", "hello"}
+
+	for _, word := range words {
+		// can include a short variable declaration
+		switch wordLen := len(word) ; {
+		case wordLen < 5:
+			fmt.Println(word, "is a short word!")
+		case wordLen > 10:
+			fmt.Println(word, "is a long word!")
+		default:
+			fmt.Println(word, "is exactly the right length.")
+		}
+	}
+
+
+	var a int = 5
+
+
+// this is no good	
+	switch {
+	case a == 2:
+		fmt.Println("a is 2")
+	case a == 3:
+		fmt.Println("a is 3")
+	case a == 4:
+		fmt.Println("a is 4")
+	default:
+		fmt.Println("otherwise, a is ", a)
+	}
+
+// this is better
+	switch a {
+	case 2:
+		fmt.Println("a is 2")
+	case 3:
+		fmt.Println("a is 3")
+	case 4:
+		fmt.Println("a is 4")
+	default:
+		fmt.Println("otherwise, a is ", a)
+	}
+
+	a = rand.Intn(10)
+	for a < 100 {
+		if a%5 == 0 {
+			goto done
+		}
+		a = a*2 + 1
+	}
+
+	fmt.Println("do something when the loop completes normally")
+	done:
+	fmt.Println("do complicated stuff no matter why we left the loop")
+	fmt.Println(a)
+
+		
+
+// The closing bracket of the main () function
+}
---
+++ learning-go-book/example5-1.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"os"
+	"strconv"
+	"time"
+	"sort"
+)
+
+func main () {
+	fmt.Println(addTo(3))
+	fmt.Println(addTo(3, 2))
+	fmt.Println(addTo(3, 2, 4, 6, 8))
+	a := []int{4, 3}
+	fmt.Println(addTo(3, a...))
+	fmt.Println(addTo(3, []int{1, 2, 3, 4, 5} ...))
+
+	 result, remainder, err := divAndRemainder(5 ,2)
+
+	 if err != nil {
+		 fmt.Println(err)
+		 os.Exit(1)
+	 }
+	 fmt.Println(result, remainder ) 
+
+	 type opFuncType func (int, int) int
+
+	var opMap = map[string] opFuncType {
+		"+": add,
+		"-": sub,
+		"*": mul,
+		"/": div,
+	}
+
+	expressions := [][]string{
+		[]string{"2", "+", "3"},
+		[]string{"2", "-", "3"},
+		[]string{"2", "*", "3"},
+		[]string{"2", "/", "3"},
+		[]string{"two", "+", "three"},
+		[]string{"5"},
+	}
+
+	for _, expression := range expressions {
+		if len(expression) != 3 {
+			fmt.Println("invalid expression:", expression)
+			continue
+		}
+
+		p1, err := strconv.Atoi(expression[0])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		op := expression[1]
+		opFunc, ok := opMap[op]
+		if !ok {
+			fmt.Println("unsupported operator:", op)
+			continue
+		}
+
+		p2, err := strconv.Atoi(expression[2])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		result := opFunc(p1, p2)
+		fmt.Println(result)
+	}
+
+	for i := 0; i < 5; i++ {
+		func(j int) {
+			fmt.Println("printing", j, "from inside of any anonymous function")
+		}(i)
+
+		func () {
+			dt := time.Now()
+			time.Sleep(time.Second)
+			fmt.Println("yet another anonymous function. Time is:", dt.Format("01-02-2006 15:04:05:06"))
+		}()
+	}
+
+	type Person struct {
+		FirstName string
+		LastName string
+		Age int
+	}
+
+	people := []Person{
+		{"Pat", "Patterson", 37},
+		{"Tracy", "Bobbert", 23},
+		{"Fred", "Fredson", 18},
+	}
+	fmt.Println(people)
+
+	sort.Slice(people, func(i int, j int) bool {
+		return people[i].LastName < people[j].LastName
+	})
+	fmt.Println(people)
+
+	sort.Slice(people, func(i int, j int) bool {
+		return people[i].Age < people[j].Age
+	})
+	fmt.Println(people)
+
+	twoBase := makeMult(2)
+	threeBase := makeMult(3)
+
+	for i := 0; i < 6; i++ {
+		fmt.Println(i, twoBase(i), threeBase(i))
+	}
+
+}
+// main ends here 
+
+func makeMult(base int) func (int) int {
+	return func(factor int) int {
+		return base * factor 
+	}
+}
+
+func addTo(base int, vals ...int)[]int {
+	out := make([]int, 0, len(vals))
+	for _, v := range vals {
+		out = append(out, base+v)
+	}
+	return out
+
+}
+
+func divAndRemainder(numerator int, denominator int) ( result int, remainder int, err error)  {
+	if denominator == 0 {
+		err =  errors.New("cannot divide by zero")
+		// no need to assign 0 to result or remainder as default is 0
+		return 
+	}
+	
+ result, remainder = numerator / denominator, numerator % denominator
+ return
+}
+
+func add (i int, j int) int {return i + j}
+func sub (i int, j int) int {return i - j}
+func mul (i int, j int) int {return i * j}
+func div (i int, j int) int {return i / j}
+
+
