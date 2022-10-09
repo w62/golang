b242243c584c39fb8c1067b1f459b1833b64ba5e
王威廉 <william@jiaozi.mynet>
2022-10-10 00:20:21 +0800

    Add a test

--- go.dev/greetings/greetings.go
+++ go.dev/greetings/greetings.go
@@ -15,6 +15,7 @@ func Hello(name string) (string, error) {
 	}
 	// Create a message using a random format.
 	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprint(randomFormat()) // for failed test
 	return message, nil
 }
 
