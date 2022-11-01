098a7dd09384a0a3f55338937eea73e5cd417399
william <william@freepackets.net>
2022-11-02 00:56:42 +0800

    Completed grain 	modified:   grains/grains.go

--- exercism/go/grains/grains.go
+++ exercism/go/grains/grains.go
@@ -1,9 +1,36 @@
 package grains
 
+import (
+	"errors"
+	"math"
+	"fmt"
+)
+
 func Square(number int) (uint64, error) {
+	var val uint64 = 1
+	var ival uint64 = 1
+	var r uint64 = 2
+	var n uint64 = uint64(number)
+
+	if number < 1 || number > 64 {
+		return 0, errors.New("invalid range")
+	}
+
+	val = ival * uint64(math.Pow(float64(r), float64(n - 1)))
+
+	return val, nil
 	panic("Please implement the Square function")
 }
 
 func Total() uint64 {
+	var val uint64 = 1
+	var ival uint64 = 1
+	var r uint64 = 2
+	
+	for i := 0 ; i <= 64 ; i ++ {
+		val +=   ival * uint64(math.Pow(float64(r), float64(i)))
+	}
+	fmt.Printf("val=%v\n", val)
+	return math.MaxUint64
 	panic("Please implement the Total function")
 }
