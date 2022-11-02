da5a69ea62a9ceca2e662e22f3d17b3c4b0ffc24
william <william@freepackets.net>
2022-11-02 23:57:23 +0800

    Working on protein
    	modified:   protein_translation.go

--- exercism/go/protein-translation/protein_translation.go
+++ exercism/go/protein-translation/protein_translation.go
@@ -1,9 +1,69 @@
 package protein
 
+import (
+	"fmt"
+	"errors"
+)
+
+const rlength = 3
+
+var ErrStop error 
+var ErrInvalidBase error 
+
 func FromRNA(rna string) ([]string, error) {
+	var codon []string
+	var protein []string
+
+	fmt.Println("rna", rna)
+
+	if len(rna) % 3 != 0 {
+		return nil, errors.New("Invalid rna")
+	}
+
+	for i := 0; i < len(rna); i += rlength {
+		fmt.Println("Here?", i)
+		codon[i] = string(rna[i]+rlength-1)
+		val, err := FromCodon(codon[i])
+		if err != nil {
+			return nil, errors.New("Invalid codon")
+		}
+//		protein[i] = val
+		protein = append(protein, val)
+		
+	}
+	fmt.Printf("codon=%v\n", codon)
+	fmt.Printf("protein=%v\n", protein)
+	return protein, nil
 	panic("Please implement the FromRNA function")
 }
 
+
 func FromCodon(codon string) (string, error) {
+	fmt.Println(codon)
+	protein := ""
+	switch {
+	case codon == "AUG":
+		protein = "Methionine"
+	case codon == "UUU" || codon == "UUC":
+		protein = "Phenylalanine"
+	case codon == "UUA" || codon == "UUG":
+		protein = "Leucine"
+	case codon == "UCU" || codon == "UCC" ||
+	codon == "UCA" || codon == "UCG":
+		protein = "Serine"
+	case codon == "UAU" || codon == "UAC":
+		protein = "Tyrosine"
+	case codon == "UGU" || codon == "UGC":
+		protein = "Cysteine"
+	case codon == "UGG":
+		protein = "Tryptophan"
+	case codon == "UAA" || codon == "UAG" ||
+	codon == "UGA" :
+		protein = "" //STOP
+	default:
+		return "" , nil
+		
+	}
+	return protein, nil
 	panic("Please implement the FromCodon function")
 }
--- myown/type/t2.go
+++ myown/type/t2.go
@@ -6,5 +6,7 @@ import (
 
 func main () {
 	var s string = "hello"
+	var s2 []string
 	fmt.Printf("s=%s type=%T type s[0]=\n", s, s, s[0])
+	fmt.Println("zero value of s2=",s2)
 }
