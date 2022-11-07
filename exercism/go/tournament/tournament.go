f3dad2c5a2d9fa52b934d79d0dd3a181c3956f85
william <william@freepackets.net>
2022-11-08 00:48:56 +0800

    Sync some work 	modified:   exercism/go/tournament/tournament.go 	modified:   exercism/go/tree-building/tree_building.go

--- exercism/go/tournament/tournament.go
+++ exercism/go/tournament/tournament.go
@@ -1,7 +1,36 @@
 package tournament
 
-import "io"
+import (
+	"io"
+	"fmt"
+	"regexp"
+)
 
 func Tally(reader io.Reader, writer io.Writer) error {
+	//txt := `^(?P<lteam>(\w|\s)+);(?P<rteam>(\w|\s)+);(?P<result>(\w|\s)+)$` 
+	txt := `^(?P<lteam>(\w|\s)+);(?P<rteam>(\w|\s)+);(?P<result>(\w|\s)+)$`
+	re := regexp.MustCompile(txt)
+	buf := ""
+
+	fmt.Println("reader=",reader)
+	if b, err := io.ReadAll(reader); err == nil {
+		buf = string(b)
+	}
+	fmt.Println("buf=",buf)
+	buf2 := "Allegoric Alaskians;Blithering Badgers;win
+	Devastating Donkeys;Courageous Californians;draw"
+/*
+Devastating Donkeys;Allegoric Alaskians;win
+Courageous Californians;Blithering Badgers;loss
+Blithering Badgers;Devastating Donkeys;loss
+Allegoric Alaskians;Courageous Californians;win"
+*/
+	match := re.FindStringSubmatch(buf2)
+	fmt.Println("match=",match)
+	  for i, name := range re.SubexpNames() {
+        fmt.Printf("'%s'\t %d -> %s\n", name, i, match[i])
+    }
+//	fmt.Println(re)
+	return nil
 	panic("Please implement the Tally function")
 }
--- exercism/go/tree-building/tree_building.go
+++ exercism/go/tree-building/tree_building.go
@@ -1,5 +1,10 @@
 package tree
 
+import (
+	"fmt"
+	"errors"
+)
+
 type Record struct {
 	ID     int
 	Parent int
@@ -11,7 +16,70 @@ type Node struct {
 	Children []*Node
 	// feel free to add fields as you see fit
 }
+func remove(slice []Record, s int) []Record {
+    return append(slice[:s], slice[s+1:]...)
+}
 
+func duplicate (slice []Record) bool {
+	if len(slice) == 1 {
+		return false
+	}
+	for i := 0; i < len(slice); i ++ {
+		for j := i+1; j < len(slice) ; j ++ {
+			if slice[i].ID == slice[j].ID &&
+			 slice[i].Parent == slice[j].Parent {
+				 fmt.Println("i=", i, "j=",j)
+				 fmt.Println("slice[i]", slice[i], "slice[j]", slice[j])
+				return true
+			}
+		}
+	}
+	return false
+}
 func Build(records []Record) (*Node, error) {
+
+	var rootNode Node
+	var rootRecord int
+
+	if len(records) <=0 {
+		return nil, nil
+	}
+
+	fmt.Println("There are", len(records), " records.")
+	fmt.Println("The entire data set is", records)
+	if duplicate(records) {
+		return nil, errors.New("Duplicate records")
+	}
+
+
+	numberOfRoots := 0
+
+	for i,r := range records {
+		if r.ID == r.Parent {
+			rootRecord = i
+			fmt.Println("in loop r=",r, " RootRecord", rootRecord)
+			numberOfRoots ++
+		}
+	}
+
+	if numberOfRoots != 1 {
+		return nil, errors.New("not one and only one root")
+	}
+			fmt.Println("RootRecord", rootRecord)
+	fmt.Println("root node is ", rootRecord, "value", records[rootRecord])
+
+
+	rootNode.ID = records[rootRecord].ID
+	records = remove(records, rootRecord)
+
+	if len(records) > 0 {
+
+	for i := 0; i < len(records) ; i ++ {
+
+	}
+} else {
+}
+
+	return &rootNode, nil
 	panic("Please implement the Build function")
 }
