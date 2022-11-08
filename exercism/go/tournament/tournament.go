f88e80446d46da03b3edce8efc9164efe7ca4f5b
william <william@freepackets.net>
2022-11-09 00:54:40 +0800

    Format error 	modified:   tournament.go

--- exercism/go/tournament/tournament.go
+++ exercism/go/tournament/tournament.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"io"
 		"regexp"
+		"sort"
 	"strings"
 )
 
-type detail struct {
-	MP int
-}
+	type score struct {
+		team string
+		score int
+	}
 
 func Tally(reader io.Reader, writer io.Writer) error {
 	//txt := `^(?P<lteam>(\w|\s)+);(?P<rteam>(\w|\s)+);(?P<result>(\w|\s)+)$`
@@ -67,13 +69,58 @@ func Tally(reader io.Reader, writer io.Writer) error {
 		}
 	}
 
-	_,_ = writer.Write( []byte("Team                            | MP |  W |  D |  L |  P\n"))
+	_,_ = writer.Write( []byte(" Team                           | MP |  W |  D |  L |  P\n"))
+
+
+	var list []score 
+	
+	for val,_ := range P {
+		var note score
+		note.team = val
+		note.score = P[val]
+		list = append(list, note)
+	}
+
+	fmt.Println("list=",list)
+
+	list = sortteam(list)
+
+	fmt.Println("list=",list)
+/*
 	for val,_ := range MP {
-		
 		s := fmt.Sprintf("%-*s |  %d |  %d |  %d |  %d |  %d\n", 30, val, MP[val], W[val], D[val], L[val], P[val])
+		_, _ = writer.Write([]byte(s))
+	}
+*/
+	for i, _ := range list {
+		s := fmt.Sprintf("%-*s |  %d |  %d |  %d |  %d |  %d\n", 30, list[i].team, MP[list[i].team], 
+		W[list[i].team], D[list[i].team], L[list[i].team], list[i].score)
 		_, _ = writer.Write([]byte(s))
 	}
 
 	return nil
+
 	panic("Please implement the Tally function")
+}
+
+func sortteam( list []score) []score {
+
+	sort.Slice(list, func(i,j int) bool {
+		if list[i].score == list[j].score {
+			return list[i].team > list[j].team
+		}
+		return list[i].score > list[j].score
+	})
+
+	/*
+
+		sort.Slice(list, func (i, j int) bool {
+			return list[i].team > list[j].team
+		})
+
+		*/
+
+		fmt.Println("sorted list=", list)
+
+	return list
 }
