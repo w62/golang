d919c8b35e4eceffc4d0295fbf067e7590088b7d
william <william@freepackets.net>
2022-11-10 23:39:25 +0800

    Completed two easy exercises. 	modified:   proverb/proverb.go 	modified:   space-age/space_age.go

--- exercism/go/proverb/proverb.go
+++ exercism/go/proverb/proverb.go
@@ -5,11 +5,46 @@
 // https://golang.org/doc/effective_go.html#commentary
 package proverb
 
+import (
+	"fmt"
+)
+
 // Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
 	// Write some code here to pass the test suite.
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
+	var result []string
+	if len(rhyme) <= 0 {
+		return nil
+	}
+	if len(rhyme) == 1 {
+
+		result = append(result, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
+	} else {
+		modern := true
+		if rhyme[0] == "pin" {
+			modern = true
+		} else {
+			modern = false
+		}
+
+		for len(rhyme) > 0 {
+			if len(rhyme) >= 2 {
+				result = append(result, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[0], rhyme[1]))
+				rhyme = rhyme[1:]
+			} else if len(rhyme) == 1 {
+				if modern {
+					result = append(result, fmt.Sprintf("And all for the want of a pin."))
+				} else {
+					result = append(result, fmt.Sprintf("And all for the want of a nail."))
+				}
+				rhyme = rhyme[1:]
+
+			}
+		}
+	}
+	return result
 	panic("Please implement the Proverb function")
 }
--- exercism/go/space-age/space_age.go
+++ exercism/go/space-age/space_age.go
@@ -1,7 +1,35 @@
 package space
+import (
+	"fmt"
+)
 
 type Planet string
 
+const earthyear = 31557600 
+
 func Age(seconds float64, planet Planet) float64 {
+	fmt.Println(planet)
+	var factor float64 
+	switch planet {
+	case "Earth":
+		factor = 1.0
+		case "Mercury":
+			factor = 0.2408467
+		case "Venus":
+			factor = 0.61519726
+		case "Mars":
+			factor = 1.8808158
+		case "Jupiter":
+			factor = 11.862615
+		case "Saturn":
+			factor = 29.447498
+		case "Uranus":
+			factor = 84.016846
+		case "Neptune":
+			factor = 164.79132
+		default:
+			return -1.0
+}
+	return seconds / (factor * earthyear)
 	panic("Please implement the Age function")
 }
