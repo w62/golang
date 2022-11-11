2b3ba875dfc762ba0043923f0b1ffebb075c82aa
william <william@freepackets.net>
2022-11-11 18:31:22 +0800

    Continue working on tree-building 	modified:   tournament/tournament.go 	modified:   tree-building/tree_building.go

--- exercism/go/tournament/tournament.go
+++ exercism/go/tournament/tournament.go
@@ -1,100 +1,104 @@
 package tournament
 
 import (
+	"errors"
 	"fmt"
 	"io"
-		"regexp"
-		"sort"
+	"regexp"
+	"sort"
 	"strings"
 )
 
-	type score struct {
-		team string
-		score int
-	}
+type score struct {
+	team  string
+	score int
+}
 
 func Tally(reader io.Reader, writer io.Writer) error {
-	//txt := `^(?P<lteam>(\w|\s)+);(?P<rteam>(\w|\s)+);(?P<result>(\w|\s)+)$`
-		txt := `^(?P<lteam>(\w|\s)+);(?P<rteam>(\w|\s)+);(?P<result>(\w|\s)+)$`
+	txt := `^(?P<lteam>(\w|\s)+);(?P<rteam>(\w|\s)+);(?P<result>(\w|\s)+)$`
 	re := regexp.MustCompile(txt)
+
 	buf := ""
 	lteam := ""
 	rteam := ""
 	state := ""
+
 	var MP map[string]int
 	var W map[string]int
 	var D map[string]int
 	var L map[string]int
 	var P map[string]int
+
 	MP = make(map[string]int)
 	W = make(map[string]int)
 	D = make(map[string]int)
 	L = make(map[string]int)
 	P = make(map[string]int)
-//	fmt.Println("reader=", reader)
+
 	if b, err := io.ReadAll(reader); err == nil {
 		buf = string(b)
+	} else {
+		return errors.New("Invalid data")
 	}
 
 	s := strings.Split(buf, "\n")
 
 	for i := 0; i < len(s); i++ {
 		if s[i] == "" {
-//			fmt.Printf("s[%d]=is empty\n", i)
 		} else {
-//			fmt.Printf("s[%d]=%s\n", i, s[i])
-			r:= re.FindStringSubmatch(s[i])
+			r := re.FindStringSubmatch(s[i])
+			if r == nil {
+				continue
+			}
 			lteam = r[1]
 			rteam = r[3]
 			state = r[5]
 
-			MP[lteam] ++
-			MP[rteam] ++
+			MP[lteam]++
+			MP[rteam]++
 			switch state {
 			case "win":
 				W[lteam] += 1
 				L[rteam] += 1
 				P[lteam] += 3
+				P[rteam] += 0 // Nevertheless generate an entry in the P map
 			case "loss":
 				W[rteam] += 1
 				L[lteam] += 1
 				P[rteam] += 3
+				P[lteam] += 0 // Nevertheless generate an entry in the P map
 			case "draw":
 				D[rteam] += 1
 				D[lteam] += 1
 				P[lteam] += 1
 				P[rteam] += 1
+			default:
+				return errors.New("Invalid data")
 			}
-//			fmt.Printf("lteam=%s rteam=%s status=%s\n\n", lteam, rteam, state)
 		}
 	}
 
-	_,_ = writer.Write( []byte(" Team                           | MP |  W |  D |  L |  P\n"))
+	// This 👇 format string is pain in the ass.
+	_, _ = writer.Write([]byte("Team                           | MP |  W |  D |  L |  P\n"))
 
+	var list []score
 
-	var list []score 
-	
-	for val,_ := range P {
+	for val, _ := range P {
 		var note score
 		note.team = val
 		note.score = P[val]
 		list = append(list, note)
 	}
 
-	fmt.Println("list=",list)
+	if list == nil {
+		return errors.New("Invalid input")
+	}
 
 	list = sortteam(list)
 
-	fmt.Println("list=",list)
-/*
-	for val,_ := range MP {
-		s := fmt.Sprintf("%-*s |  %d |  %d |  %d |  %d |  %d\n", 30, val, MP[val], W[val], D[val], L[val], P[val])
-		_, _ = writer.Write([]byte(s))
-	}
-*/
 	for i, _ := range list {
-		s := fmt.Sprintf("%-*s |  %d |  %d |  %d |  %d |  %d\n", 30, list[i].team, MP[list[i].team], 
-		W[list[i].team], D[list[i].team], L[list[i].team], list[i].score)
+		s := fmt.Sprintf("%-*s |  %d |  %d |  %d |  %d |  %d\n", 30, list[i].team, MP[list[i].team],
+			W[list[i].team], D[list[i].team], L[list[i].team], list[i].score)
 		_, _ = writer.Write([]byte(s))
 	}
 
@@ -103,24 +107,14 @@ func Tally(reader io.Reader, writer io.Writer) error {
 	panic("Please implement the Tally function")
 }
 
-func sortteam( list []score) []score {
+func sortteam(list []score) []score {
 
-	sort.Slice(list, func(i,j int) bool {
+	sort.Slice(list, func(i, j int) bool {
 		if list[i].score == list[j].score {
-			return list[i].team > list[j].team
+			return list[i].team < list[j].team
 		}
 		return list[i].score > list[j].score
 	})
-
-	/*
-
-		sort.Slice(list, func (i, j int) bool {
-			return list[i].team > list[j].team
-		})
-
-		*/
-
-		fmt.Println("sorted list=", list)
 
 	return list
 }
--- exercism/go/tree-building/tree_building.go
+++ exercism/go/tree-building/tree_building.go
@@ -1,8 +1,9 @@
 package tree
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
+	"sort"
 )
 
 type Record struct {
@@ -16,20 +17,21 @@ type Node struct {
 	Children []*Node
 	// feel free to add fields as you see fit
 }
+
 func remove(slice []Record, s int) []Record {
-    return append(slice[:s], slice[s+1:]...)
+	return append(slice[:s], slice[s+1:]...)
 }
 
-func duplicate (slice []Record) bool {
+func duplicate(slice []Record) bool {
 	if len(slice) == 1 {
 		return false
 	}
-	for i := 0; i < len(slice); i ++ {
-		for j := i+1; j < len(slice) ; j ++ {
+	for i := 0; i < len(slice); i++ {
+		for j := i + 1; j < len(slice); j++ {
 			if slice[i].ID == slice[j].ID &&
-			 slice[i].Parent == slice[j].Parent {
-				 fmt.Println("i=", i, "j=",j)
-				 fmt.Println("slice[i]", slice[i], "slice[j]", slice[j])
+				slice[i].Parent == slice[j].Parent {
+				fmt.Println("i=", i, "j=", j)
+				fmt.Println("slice[i]", slice[i], "slice[j]", slice[j])
 				return true
 			}
 		}
@@ -38,48 +40,148 @@ func duplicate (slice []Record) bool {
 }
 func Build(records []Record) (*Node, error) {
 
-	var rootNode Node
+	var root *Node
 	var rootRecord int
 
-	if len(records) <=0 {
+	//check for 0 or zero records
+	if len(records) <= 0 {
 		return nil, nil
 	}
 
+	// Print out the initial data for verification
 	fmt.Println("There are", len(records), " records.")
 	fmt.Println("The entire data set is", records)
 	if duplicate(records) {
 		return nil, errors.New("Duplicate records")
 	}
 
+	// number of root checking
+	// There should be one and only one root, no more no less
 
 	numberOfRoots := 0
 
-	for i,r := range records {
+	for i, r := range records {
+		fmt.Printf("r.ID=%d r.Parent=%d\n", r.ID, r.Parent)
 		if r.ID == r.Parent {
 			rootRecord = i
-			fmt.Println("in loop r=",r, " RootRecord", rootRecord)
-			numberOfRoots ++
+			fmt.Println("in loop r=", r, " RootRecord", rootRecord)
+			numberOfRoots++
+		}
+
+		if r.ID != 0 && r.Parent != 0 {
+			if r.ID > r.Parent {
+			return nil, errors.New("ID larger than Parent")
+		}
 		}
 	}
 
 	if numberOfRoots != 1 {
 		return nil, errors.New("not one and only one root")
 	}
-			fmt.Println("RootRecord", rootRecord)
+	// end of number of root checking
+
+	fmt.Println("RootRecord", rootRecord)
 	fmt.Println("root node is ", rootRecord, "value", records[rootRecord])
 
+	n := Node{
+		ID:       records[rootRecord].ID,
+		Children: nil,
+	}
 
-	rootNode.ID = records[rootRecord].ID
 	records = remove(records, rootRecord)
 
-	if len(records) > 0 {
+	// If the root is not the only node, then construct the tree
 
-	for i := 0; i < len(records) ; i ++ {
+	if records != nil {
+		kids := children(records, 0)
 
+		n.Children = kids
+		fmt.Printf("kids=%v records=%v\n", n.Children, records)
 	}
-} else {
+
+	root = &n
+
+	/*
+
+
+		var tree map[int]int
+
+		tree = make(map[int]int)
+
+
+
+		for i := 0; i <len(records); i++ {
+			fmt.Printf("%d ID=%d Parent=%d\n",i, records[i].ID, records[i].Parent)
+			tree [records[i].Parent]++
+		}
+
+		fmt.Println("tree map[int]int=", tree)
+
+		for val, _ := range  tree {
+
+		fmt.Printf("Parent %d has %d childs.\n",val, tree[val])
+	}
+
+		records = sortrecord(records)
+		fmt.Println("After removing root and sort, there are", len(records), " records. Data ", records)
+
+
+		for len(records) > 0 {
+
+			for i := 0; i < len(records); i++ {
+				if records[i].Parent == (*next).ID{
+					n := Node{
+						ID:       records[i].ID,
+						Children: nil,
+					}
+					fmt.Println("n=",n)
+
+					kids := children(records,(*next).ID)
+					(*next).Children = append((*next).Children, &n)
+					fmt.Println("number kids=", len(kids), " kids=", kids)
+
+					if kids == nil {
+
+					next = (*next).Children[0]
+				}
+			fmt.Println("before records=", records)
+					records = remove(records, i)
+			fmt.Println("after records=", records)
+
+				}
+			}
+		}*/
+	return root, nil
+	panic("Please implement the Build function")
 }
 
-	return &rootNode, nil
-	panic("Please implement the Build function")
+func children(list []Record, pid int) []*Node {
+
+	var child []*Node
+	j := len(list)
+
+	for i := 0; i < len(list); i++ {
+
+		if list[i].Parent == pid {
+			n := Node{
+				ID:       list[i].ID,
+				Children: nil,
+			}
+			child = append(child, &n)
+			list = remove(list, i)
+		}
+	}
+	fmt.Println("child=", child)
+	return child
+}
+
+func sortrecord(list []Record) []Record {
+	sort.Slice(list, func(i, j int) bool {
+		if list[i].Parent == list[j].Parent {
+			return list[i].ID < list[j].ID
+		}
+		return list[i].Parent < list[j].Parent
+	})
+
+	return list
 }
