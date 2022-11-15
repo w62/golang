9a57d6545b357e343136f0229012fd38350d94e7
william <william@freepackets.net>
2022-11-16 07:50:42 +0800

    Completed twelve days
    	modified:   ../robot-name/robot_name.go
    	modified:   twelve_days.go

--- exercism/go/robot-name/robot_name.go
+++ exercism/go/robot-name/robot_name.go
@@ -1,11 +1,64 @@
+// Failed exercise
+// Really don't exactly what the expected problem is
+// It is a bit more on understand the problem rather than the Go language
 package robotname
 
+import (
+	"fmt"
+	
+	"math/rand"
+//	"bytes"
+	"time"
+	"sync"
+)
+
+var namelist = map[string]int{}
 // Define the Robot type here.
+type Robot struct {
+	name string
+}
+
 
 func (r *Robot) Name() (string, error) {
+
+//	fmt.Println("len(namelist)=",len(namelist))
+
+	name := randomString(2)+ fmt.Sprintf("%03d",randomInt(0, 999))
+
+	_, chk := namelist[name]
+	if  chk {
+		return "", fmt.Errorf("Name %s reissued after %d robots.", name, len(namelist))
+	} else {
+		namelist[name] = 0
+	}
+
+	return name, nil
 	panic("Please implement the Name function")
 }
 
 func (r *Robot) Reset() {
+	r.name = ""
+	return
 	panic("Please implement the Reset function")
 }
+
+
+func randomString(l int) string {
+    bytes := make([]byte, l)
+    for i := 0; i < l; i++ {
+        bytes[i] = byte(randomInt(65, 90))
+    }
+    return string(bytes)
+}
+
+func randomInt(min int, max int) int {
+	rand.Seed(time.Now().UnixNano())  
+    var mu sync.Mutex  
+    mu.Lock()
+	result := rand.Intn(max-min)
+	mu.Unlock()
+	return min + result
+}
+
+
+
--- exercism/go/twelve-days/twelve_days.go
+++ exercism/go/twelve-days/twelve_days.go
@@ -1,9 +1,60 @@
 package twelve
 
+import (
+// "fmt"
+)
+
 func Verse(i int) string {
+	val := ""
+	switch i {
+	/*	case 1:
+			val = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+		case 2:
+			val = "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree." */
+	case 1:
+		val = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+	case 2:
+		val = "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree."
+	case 3:
+		val = "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	case 4:
+		val = "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	case 5:
+		val = "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	case 6:
+		val = "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	case 7:
+		val = "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	case 8:
+		val = "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	case 9:
+		val = "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	case 10:
+		val = "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	case 11:
+		val = "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	case 12:
+		val = "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+	default:
+		val = ""
+	}
+	return val
 	panic("Please implement the Verse function")
 }
 
 func Song() string {
+	str := "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\nOn the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+	str += "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+
+	return str
 	panic("Please implement the Song function")
 }
